f3ec9d561b01cb312b496547aad6626f8269a038
goodjm0698 <goodjm0698@gmail.com>
2023-02-15 00:02:36 +0900

    test-jaemin

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reloadhello world
+          Edit <code>src/App.js</code> and
         </p>
         <a
           className="App-link"
